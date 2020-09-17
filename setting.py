a1bae2c6fadb48be13ebe6bd219fbef261952ee4
tomo-taku <tmrtkm1227tksx@gmail.com>
2020-09-17 13:38:25 +0900

    add selenium

--- setting.py
+++ setting.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-# dotenv_path = join(dirname(__file__), '.env')
-# load_dotenv(dotenv_path)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
